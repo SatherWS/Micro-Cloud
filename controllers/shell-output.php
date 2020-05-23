e49577e5b462122152830352f982aef42afc00c5
colin <satherc@go.stockton.edu>
2020-05-23 19:31:51 -0400

    dev server testing

--- controllers/shell-output.php
+++ controllers/shell-output.php
@@ -2,3 +2,8 @@
     $output = shell_exec("ls -l /media/usb/share/Music");
     echo "<pre>$output</pre>";
 ?>
+<audio controls>
+  <source src="\media\usb\share\Music\'A Horse with No Name.ogg'" type="audio/ogg">
+  <source src="\media\usb\share\Music\'A Horse with No Name.mp3'" type="audio/mpeg">
+Your browser does not support the audio element.
+</audio>
