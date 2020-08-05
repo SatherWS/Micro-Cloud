c55ffc910d28cc8a957e5cc1541a22edd027ef49
colin <satherc@go.stockton.edu>
2020-08-05 02:02:39 -0400

    updated landing page changed format of posts

--- views/logs.php
+++ views/logs.php
@@ -56,23 +56,21 @@
         <form id="notes" action="./journal-details.php" method="post">
             <table class="data journal-tab">
                 <tr class="tbl-head">
-                    <th>ID</th>
+                    <th>CATEGORY</th>
                     <th>SUBJECT</th>
                     <th>PREVIEW</th>
                     <th>CREATOR</th>
                     <th>TEAM</th>
-                    <th>CATEGORY</th>
                     <th>DATE & TIME CREATED</th>
                 </tr>
                 <?php
                     while($row = mysqli_fetch_assoc($result)) {
                         $id = $row['id'];
-                        echo "<tr onclick='myFunction($id)' name='btn-submit' value='".$row["id"]."'> <td>". $row["id"]. "</td>";
+                        echo "<tr onclick='myFunction($id)' name='btn-submit' value='".$row["id"]."'> <td>". $row["category"]. "</td>";
                         echo "<td>". $row["subject"]. "</td>";
                         echo "<td>".strip_tags($row["preview"], '<br><b><i>'). "...</td>";
                         echo "<td>".$row["creator"]."</td>";
                         echo "<td>".$row["team_name"]."</td>";
-                        echo "<td>". $row["category"]. "</td>";
                         echo "<td>".$row["date_created"]."</td></tr>";
                     }
                     /*
