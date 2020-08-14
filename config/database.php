f6446d4ae11fcd4070a2ef9c22ace9acc4be2268
root <satherwebservices@gmail.com>
2020-08-14 16:11:06 +0000

    deployment fixes 1.0.0

--- config/database.php
+++ config/database.php
@@ -1,17 +1,16 @@
 <?php
     class Database {
-        // TODO: Use system variables instead
+        /* For development
         private $server = "localhost";
         private $db = "lhapps";
         private $user = "root";
         private $pass = "mysql";
-        
-        // use when working on personal computer
-        //private $pass = "root";
-        //private $db = "note_web";
+        */
+	$db_params = parse_ini_file(dirname(__FILE__).'/db_params.ini', false);
         
         public function getConnection(){
-            $curs = new mysqli($this->server, $this->user, $this->pass, $this->db);
+            $curs = new mysqli($db_params["host"], $db_params["user"], $db_parms["password"], $db_params["dbname"]);
+
             if ($curs->connect_error) {
                 die("Connection failed: " . $curs->connect_error);
             }
--- config/ddl_config.sql
+++ config/ddl_config.sql
@@ -13,18 +13,11 @@
 -- Database should be called grooper or something
 --
 
---CREATE DATABASE if not exists note_web;
---USE note_web;
---DROP TABLE IF EXISTS journal;
-DROP TABLE IF EXISTS todo_list;
-
 create table teams (
   id int primary key auto_increment,
   team_name varchar(50) not null unique,
   admin varchar(75) not null,
-  date_created datetime default current_timestamp,
-  -- NEW: 7/28/2020
-  foreign key(admin) references users(email)
+  date_created datetime default current_timestamp
 );
 
 create table users (
@@ -49,10 +42,6 @@ create table invites (
   foreign key(team_name) references teams(team_name)
 );
 
--- Will need to add user foreign keys to the following tables
--- journal
--- todo_list
--- file paths/shortcodes? (not created yet)
 
 CREATE TABLE journal (
   id int(11) primary key auto_increment,
@@ -83,12 +72,11 @@ CREATE TABLE comments (
 --
 
 CREATE TABLE todo_list (
-	id int primary key auto_increment NOT NULL,
+	id int primary key auto_increment,
 	title varchar(45) NOT NULL,
   description varchar(100),
   status varchar(30) DEFAULT "Not Started",
 	deadline date NOT NULL,
-  --time_due time NOT NULL,
   task_repeat varchar(10) NULL,
 	importance varchar(10) NOT NULL,
   assignee varchar(50),
@@ -100,56 +88,3 @@ CREATE TABLE todo_list (
   foreign key (team_name) references teams(team_name)
 );
 
-/* 
-*   The tables below are for the bonus applications and 
-*   may not be included in the live website. 
-*/
-
---
--- Table structure for table `topics`
---
-
-CREATE TABLE `polls` (
-  `id` int(11) primary key auto_increment,
-  `admin` varchar(30) NOT NULL,
-  `topic` varchar(100) NOT NULL,
-  `date_created` datetime DEFAULT CURRENT_TIMESTAMP
-);
-
---
--- Table structure for table `votes`
---
-
-CREATE TABLE `votes` (
-  `id` int(11) primary key auto_increment not null,
-  `topic_id` int(11) not null,
-  `username` varchar(20) not null,
-  `vote` varchar(10) not null,
-  `date_created` datetime DEFAULT CURRENT_TIMESTAMP,
-  foreign key (topic_id) references polls(id)
-);
-
-
-CREATE TABLE `chatroom` (
-  `id` int(11) NOT NULL primary key auto_increment,
-  `subject` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `creator` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `time_created` datetime DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL primary key auto_increment,
-  `name` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `msg` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `time_submitted` datetime DEFAULT CURRENT_TIMESTAMP,
-  foreign key (room_id) references chatroom(id)
-);
-
-create table ratings (
-  id int primary key auto_increment not null,
-  rating int(11),
-  user_email varchar(75) not null unique,
-  date_created datetime DEFAULT CURRENT_TIMESTAMP,
-  foreign key (user_email) references users(email)
-);
