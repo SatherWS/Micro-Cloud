182b8552e945098711c929f58aabfbb348842df3
colin <satherc@go.stockton.edu>
2020-08-14 12:19:39 -0400

    fixes version 1.0.0

--- config/database.php
+++ config/database.php
@@ -6,10 +6,10 @@
         private $user = "root";
         private $pass = "mysql";
         */
-	$db_params = parse_ini_file(dirname(__FILE__).'/db_params.ini', false);
         
         public function getConnection(){
-            $curs = new mysqli($db_params["host"], $db_params["user"], $db_parms["password"], $db_params["dbname"]);
+	        $db_params = parse_ini_file(dirname(__FILE__).'/db_params.ini', false);
+            $curs = new mysqli($db_params["host"], $db_params["user"], $db_params["password"], $db_params["dbname"]);
 
             if ($curs->connect_error) {
                 die("Connection failed: " . $curs->connect_error);
