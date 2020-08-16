d6b326f3e71f62f652a06ec3a24c152492838f88
colin <satherc@go.stockton.edu>
2020-08-16 18:49:25 -0400

    fixed stuff

--- config/database.php
+++ config/database.php
@@ -1,8 +1,13 @@
 <?php
+
     class Database {
+        private $hostname = "localhost";
+        private $user = "root";
+        private $database = "swoop_team";
+        private $password = "1m0r3_projde@th";
+        
         public function getConnection(){
-	        $db_params = parse_ini_file(dirname(__FILE__).'/db_params.ini', false);
-            $curs = new mysqli($db_params["host"], $db_params["user"], $db_params["password"], $db_params["dbname"]);
+            $curs = new mysqli($this->hostname, $this->user, $this->password, $this->database);
 
             if ($curs->connect_error) {
                 die("Connection failed: " . $curs->connect_error);
