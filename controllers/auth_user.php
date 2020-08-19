937c3da3bff949e39bc07160067cf992aa5fb0b8
colin <satherc@go.stockton.edu>
2020-08-19 15:02:42 -0400

    VSCode test

--- controllers/auth_user.php
+++ controllers/auth_user.php
@@ -48,54 +48,53 @@ function search_team($curs, $team) {
 // check which radio is selected and make sure the team exists or can be created
 // below if statement is an attempt at a minimum password length
 
-function create_teamuser() {
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_user"])) {
-        if ($_POST["radio"] == "join" && search_team($curs, $_POST["team"])) {
-            $sql = "insert into users(email, team, username, pswd) values(?,?,?,?)";
-            $stmnt = mysqli_prepare($curs, $sql);
-            $hash = password_hash($_POST["pswd"], PASSWORD_BCRYPT);
-            $stmnt -> bind_param("ssss", $_POST["email"], $_POST["team"], $_POST["usr"], $hash);
-            $stmnt -> execute();
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_user"])) {
+    if ($_POST["radio"] == "join" && search_team($curs, $_POST["team"])) {
+        $sql = "insert into users(email, team, username, pswd) values(?,?,?,?)";
+        $stmnt = mysqli_prepare($curs, $sql);
+        $hash = password_hash($_POST["pswd"], PASSWORD_BCRYPT);
+        $stmnt -> bind_param("ssss", $_POST["email"], $_POST["team"], $_POST["usr"], $hash);
+        $stmnt -> execute();
 
-            // create session and launch application
+        // create session and launch application
+        $_SESSION["unq_user"] = $_POST["email"];
+        $_SESSION["user"] = $_POST["usr"];
+        $_SESSION["team"] = $_POST["team"];
+        header("Location: ../views/dashboard.php");
+    }
+    else if ($_POST["radio"] == "create" && !search_team($curs, $_POST["team"])){
+        // add team to database, then add user account
+        $sql = "insert into teams(team_name) values (?)";
+        $stmnt = mysqli_prepare($curs, $sql);
+        $stmnt -> bind_param("s", $_POST["team"]);
+        $stmnt -> execute();
+
+        // create user account
+        $sql = "insert into users(email, team, username, pswd) values(?,?,?,?)";
+        $stmnt = mysqli_prepare($curs, $sql);
+        $hash = password_hash($_POST["pswd"], PASSWORD_BCRYPT);
+        $stmnt -> bind_param("ssss", $_POST["email"], $_POST["team"], $_POST["usr"], $hash);
+        $stmnt -> execute();
+
+        // create session and launch application
+        if ($stmnt -> execute()) {
             $_SESSION["unq_user"] = $_POST["email"];
             $_SESSION["user"] = $_POST["usr"];
             $_SESSION["team"] = $_POST["team"];
             header("Location: ../views/dashboard.php");
         }
-        else if ($_POST["radio"] == "create" && !search_team($curs, $_POST["team"])){
-            // add team to database, then add user account
-            $sql = "insert into teams(team_name) values (?)";
-            $stmnt = mysqli_prepare($curs, $sql);
-            $stmnt -> bind_param("s", $_POST["team"]);
-            $stmnt -> execute();
-
-            // create user account
-            $sql = "insert into users(email, team, username, pswd) values(?,?,?,?)";
-            $stmnt = mysqli_prepare($curs, $sql);
-            $hash = password_hash($_POST["pswd"], PASSWORD_BCRYPT);
-            $stmnt -> bind_param("ssss", $_POST["email"], $_POST["team"], $_POST["usr"], $hash);
-            $stmnt -> execute();
-
-            // create session and launch application
-            if ($stmnt -> execute()) {
-                $_SESSION["unq_user"] = $_POST["email"];
-                $_SESSION["user"] = $_POST["usr"];
-                $_SESSION["team"] = $_POST["team"];
-                header("Location: ../views/dashboard.php");
-            }
-        }
-        // error messages: team dne or user non unique
-        else if ($_POST["radio"] == "join" && !search_team($curs, $_POST["team"])) {
-            $msg = "Error: team does not exist";
-            header("Location: ../authentication/signup.php?error='$msg'");
-        }
-        else if ($_POST["radio"] == "create" && search_team($curs, $_POST["team"])){
-            header("Location: ../authentication/signup.php?error="."Error: email accounts must be unique");
-        }
+    }
+    // error messages: team dne or user non unique
+    else if ($_POST["radio"] == "join" && !search_team($curs, $_POST["team"])) {
+        $msg = "Error: team does not exist";
+        header("Location: ../authentication/signup.php?error='$msg'");
+    }
+    else if ($_POST["radio"] == "create" && search_team($curs, $_POST["team"])){
+        header("Location: ../authentication/signup.php?error="."Error: email accounts must be unique");
     }
 }
 
+/*
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_user"])) {
     if ($_POST["radio"] == "skip" && !check_users()) {
         $sql = "insert into users(email, username, password) values(?, ?, ?)";
@@ -117,7 +116,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["add_user"])) {
         create_teamuser();
     }
 }
-
+*/
 /*
 *   Invite user to team via settings.php [POSSIBLY MOVE THIS TO A NEW CONTROLLER]
 */
